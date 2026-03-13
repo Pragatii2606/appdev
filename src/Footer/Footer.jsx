5ef01bc1722dfefa1713fe50aad1ed7b1ac7e2c8
Pragatii2606 <youremail@example.com>
2026-03-13 09:43:25 +0530

    fix build error

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import "./footer.css";
+import "./Footer.css";
 
  function Footer() {
   const logoUrl = "https://res.cloudinary.com/daup99ghe/image/upload/v1773281305/Kds_logo_white_1_dljerq.png";
